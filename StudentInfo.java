72c546975414fe937a26c9e6d6d3b79e9919f5c1
raihan243026 <raihanchowdhury20004@gmail.com>
2025-05-18 23:29:34 +0600

    Add files via upload

---
+++ StudentInfo.java
@@ -0,0 +1,20 @@
+public class StudentInfo {
+
+    public static void main(String[] args) {
+        String name = "Raihan";
+        String studentId = "E243026";
+        int age = 19;
+        String email = "raihan243026@gmail.com";
+        String phone = "01936705193";
+        String address = "Agrabad";
+
+        System.out.println("--- Student Information ---");
+        System.out.println("Name: " + name);
+        System.out.println("Student ID: " + studentId);
+        System.out.println("Age: " + age);
+        System.out.println("Email: " + email);
+        System.out.println("Phone: " + phone);
+        System.out.println("Address: " + address);
+        System.out.println("-------------------------");
+    }
+}
